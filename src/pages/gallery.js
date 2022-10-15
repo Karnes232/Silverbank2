665645474d41076d71b200e2c6727f56d1cc0c09
James Karnes <karnes.james@gmail.com>
2022-10-15 19:00:45 -0400

    added canonical to gallery page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import "../scss/main.scss"
 import SEO from "../components/seo"
+import Helment from "react-helmet"
 import Navbar from "../components/Navbar"
 import Gallery from "../components/Gallery"
 
@@ -8,6 +9,9 @@ import Gallery from "../components/Gallery"
 const GalleryPage = () => {
   return (
     <main>
+      <Helment>
+        <link rel="canonical" href="https://www.silverbankwhales.com/gallery" />
+      </Helment>
       <SEO title="Photo Gallery" />
       <Navbar />
       <Gallery />
