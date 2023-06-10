597a43d6fa8098fafbc49a5672657102776ed413
James Karnes <karnes.james@gmail.com>
2023-06-10 10:43:14 -0400

    testing

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,19 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import video from "../images/grandbay.mp4"
 
+import videoSmall from "../images/grandbay-small.mp4"
+
 const Packages = () => {
+  const [width, setWidth] = useState()
+  useEffect(() => {
+    setWidth(window.innerWidth)
+  }, [])
+  console.log(width)
   return (
-    <section class="section-package u-margin-bottom-medium" id="tours">
-      <div class="bg-video">
-        <video
-          className="bg-video__content"
-          autoPlay
-          muted
-          loop
-          playsInline
-          src={video}
-        >
-          <source src={video} type="video/mp4" />
+    <section className="section-package u-margin-bottom-medium" id="tours">
+      <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop playsInline>
+          {width > 600 ? (
+            <source src={video} type="video/mp4" />
+          ) : (
+            <source src={videoSmall} type="video/mp4" />
+          )}
           Your device does not support playing 'video/mp4' videos
         </video>
       </div>
